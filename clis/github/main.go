79f2210e8a7585fc9b6e95c9e56bbd7b6cb1eaa8
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-01-12 21:22:12 +0900

    Add following API

--- clis/github/main.go
+++ clis/github/main.go
@@ -14,7 +14,10 @@ var (
 )
 
 var (
-	userName = flag.String("u", "", "user name")
+	userName  = flag.String("u", "", "user name")
+	apiType   = flag.String("t", "", "use this API type")
+	repoLists = "list"
+	followers = "following"
 )
 
 func main() {
@@ -25,18 +28,34 @@ func main() {
 func doMain() {
 	flag.Parse()
 	client := github.NewClient(nil)
-	opt := &github.RepositoryListOptions{Type: "public"}
-	if *userName == "" {
-		fmt.Fprintf(os.Stderr, "the given user name is empty\n")
+	switch *apiType {
+	case repoLists:
+		opt := &github.RepositoryListOptions{Type: "public"}
+		if *userName == "" {
+			fmt.Fprintf(os.Stderr, "the given user name is empty\n", *userName)
+			exitCode = 2
+			return
+		}
+		repos, resp, err := client.Repositories.List(context.Background(), *userName, opt)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to get list of repositories: %#v\n", err)
+			exitCode = 2
+			return
+		}
+		defer resp.Body.Close()
+		fmt.Printf("First repo: %v\n, Rate: %s\n", repos[0], resp.Rate.String())
+	case followers:
+		users, resp, err := client.Users.ListFollowing(context.Background(), *userName, nil)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to get list of following: %#v\n", err)
+			exitCode = 2
+			return
+		}
+		defer resp.Body.Close()
+		fmt.Printf("First following: %v\n, Rate: %s\n", users[0], resp.Rate.String())
+	default:
+		fmt.Fprintf(os.Stderr, "Invalid type: %s\n", *apiType)
 		exitCode = 2
 		return
 	}
-	repos, resp, err := client.Repositories.List(context.Background(), *userName, opt)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to get list of repositories: %#v\n", err)
-		exitCode = 2
-		return
-	}
-	defer resp.Body.Close()
-	fmt.Printf("First repo: %v\n, Rate: %s\n", repos[0], resp.Rate.String())
 }
