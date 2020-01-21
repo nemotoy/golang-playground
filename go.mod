f345f70d93e0bbea74db74a79a5ae3ed14886d17
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-01-21 16:26:44 +0900

    Use cenkalti/backoff

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/nemotoy/golang-playground
 go 1.13
 
 require (
+	github.com/cenkalti/backoff v2.2.1+incompatible
 	github.com/google/go-github/v28 v28.1.1
 	github.com/mitchellh/hashstructure v1.0.0
 	github.com/olekukonko/tablewriter v0.0.4
--- http/retry/retry_test.go
+++ http/retry/retry_test.go
@@ -7,12 +7,11 @@ import (
 	"net/http/httptest"
 	"reflect"
 	"testing"
+
+	"context"
+
+	"github.com/cenkalti/backoff"
 )
-
-/*
-https://github.com/cenkalti/backoff
-
-*/
 
 var handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello")
@@ -23,11 +22,19 @@ func Test_(t *testing.T) {
 	defer ts.Close()
 
 	c := ts.Client()
-	r, err := c.Get(ts.URL)
+	var r *http.Response
+	ctx := context.Background()
+	err := backoff.Retry(func() (err error) {
+		r, err = c.Get(ts.URL)
+		if err == nil && r.StatusCode == http.StatusOK {
+			return nil
+		}
+		r.Body.Close()
+		return err
+	}, backoff.WithContext(backoff.NewExponentialBackOff(), ctx))
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer r.Body.Close()
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		t.Fatal(err)
