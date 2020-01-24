57507b869003726dd4d53a06eb233dbbc8f121f0
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-01-24 09:13:18 +0900

    Fix field names simply

--- flag/flag_test.go
+++ flag/flag_test.go
@@ -5,46 +5,54 @@ import (
 	"os"
 	"reflect"
 	"testing"
-	"time"
 )
 
 type opt struct {
-	name string
-	age  int
-	is   bool
-	dur  time.Duration
+	s string
+	i int
+	b bool
 }
 
-func parse(args []string) (opt, error) {
-	var o = opt{}
-	f := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	f.StringVar(&o.name, "n", "", "name")
-	f.IntVar(&o.age, "a", 0, "age")
-	f.BoolVar(&o.is, "b", false, "enable xxx")
-	f.DurationVar(&o.dur, "d", 1*time.Second, "duration")
-	return o, f.Parse(args)
+func parse(args []string) (*opt, error) {
+	var o = &opt{}
+	f := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	f.StringVar(&o.s, "n", "", "string flag")
+	f.IntVar(&o.i, "a", 0, "int flag")
+	f.BoolVar(&o.b, "b", false, "bool flag")
+	if err := f.Parse(args); err != nil {
+		return nil, err
+	}
+	return o, nil
 }
 
 func Test_parse(t *testing.T) {
 	tests := []struct {
-		name string
-		in   []string
-		want opt
+		name    string
+		in      []string
+		want    *opt
+		wantErr bool
 	}{
 		{
-			name: "",
-			in:   []string{"-n", "nameA", "-a", "20", "-b", "true", "-d", "1s"},
-			want: opt{name: "nameA", age: 20, is: true, dur: 1 * time.Second},
+			name:    "correct args",
+			in:      []string{"-n", "sss", "-a", "100", "-b", "true"},
+			want:    &opt{s: "sss", i: 100, b: true},
+			wantErr: false,
+		},
+		{
+			name:    "empty args",
+			in:      []string{},
+			want:    &opt{s: "", i: 0, b: false},
+			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := parse(tt.in)
-			if err != nil {
-				t.Fatal(err)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parse() error = %v, wantErr %v", err, tt.wantErr)
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("got = %+v, but want = %+v", got, tt.want)
+				t.Errorf("parse() got = %+v, but want = %+v", got, tt.want)
 			}
 		})
 	}
