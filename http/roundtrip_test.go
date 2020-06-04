5fdedacb823af7ad07f090eccc2ec810bfd58b90
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-06-04 16:58:41 +0900

    chore: add content-type into response headers

--- http/roundtrip_test.go
+++ http/roundtrip_test.go
@@ -9,6 +9,7 @@ import (
 
 func Test_sampleRoundtrip(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain")
 		w.WriteHeader(200)
 		w.Write([]byte("success!"))
 	}))
@@ -24,6 +25,10 @@ func Test_sampleRoundtrip(t *testing.T) {
 	if resp.StatusCode != 200 {
 		t.Fatalf("expected 200, got %d", resp.StatusCode)
 	}
+	if resp.Header.Get("Content-Type") != "text/plain" {
+		t.Fatalf("expected text/plain, got %d", resp.StatusCode)
+	}
+
 	if v, err := ioutil.ReadAll(resp.Body); err != nil {
 		t.Fatal(err)
 	} else if string(v) != "success!" {
