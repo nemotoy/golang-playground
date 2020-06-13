feaf56085f8a109bb485d8e0d3ae78d398866475
nemotoy <nemotoy@users.noreply.github.com>
2020-06-14 08:37:10 +0900

    feat: implement ratelimit API (#15)
    * feat: add dummy api and caller
    
    * feat: add simple ratelimit to api
    
    * feat: add rateLimiter interface
    
    * chore: implement per function
    
    * fix: typo
    
    * chore: improve
    
    * fix: mv dir

--- go.mod
+++ go.mod
@@ -12,4 +12,5 @@ require (
 	go.uber.org/multierr v1.5.0
 	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
 	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
+	golang.org/x/time v0.0.0-20170424234030-8be79e1e0910
 )
---
+++ rate/api/rate.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"sort"
+	"sync"
+	"time"
+
+	"golang.org/x/time/rate"
+)
+
+// rf. ratelimit in https://github.com/kat-co/concurrency-in-go-src
+func main() {
+	defer log.Printf("Done.")
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.Ltime | log.LUTC)
+
+	apiConnection := Open()
+	var wg sync.WaitGroup
+	wg.Add(20)
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			if err := apiConnection.ReadFile(context.Background()); err != nil {
+				log.Printf("cannot ReadFile: %v", err)
+			}
+			log.Printf("ReadFile")
+		}()
+	}
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			if err := apiConnection.ResolveAddress(context.Background()); err != nil {
+				log.Printf("cannot ResolveAddress: %v", err)
+			}
+			log.Printf("ResolveAddress")
+		}()
+	}
+
+	wg.Wait()
+}
+
+type APIConnection struct {
+	networkLimit,
+	diskLimit,
+	apiLimit RateLimiter
+}
+
+func Open() *APIConnection {
+	return &APIConnection{
+		// limit is speed to refill tokens, b is a depth of a bucket
+		apiLimit: MultiLimiter(
+			rate.NewLimiter(Per(2, time.Second), 1),
+			rate.NewLimiter(Per(10, time.Minute), 10),
+		),
+		diskLimit: MultiLimiter(
+			rate.NewLimiter(rate.Limit(1), 1),
+		),
+		networkLimit: MultiLimiter(
+			rate.NewLimiter(Per(3, time.Second), 3),
+		),
+	}
+}
+
+func (a *APIConnection) ReadFile(ctx context.Context) error {
+	if err := MultiLimiter(a.apiLimit, a.diskLimit).Wait(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *APIConnection) ResolveAddress(ctx context.Context) error {
+	if err := MultiLimiter(a.apiLimit, a.networkLimit).Wait(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+
+func Per(everyCount int, duration time.Duration) rate.Limit {
+	return rate.Every(duration / time.Duration(everyCount))
+}
+
+type RateLimiter interface {
+	Wait(ctx context.Context) error
+	Limit() rate.Limit
+}
+
+type multiLimiter struct {
+	limiters []RateLimiter
+}
+
+func MultiLimiter(limiters ...RateLimiter) RateLimiter {
+	byLimit := func(i, j int) bool {
+		return limiters[i].Limit() < limiters[j].Limit()
+	}
+	sort.Slice(limiters, byLimit)
+	return &multiLimiter{limiters: limiters}
+}
+
+func (l *multiLimiter) Wait(ctx context.Context) error {
+	for _, l := range l.limiters {
+		if err := l.Wait(ctx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *multiLimiter) Limit() rate.Limit {
+	return l.limiters[0].Limit()
+}
