38b765291dc9c79541dd4314200be0cf236931f8
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-06-19 22:27:23 +0900

    chore: add bench

--- http/roundtrip/chain/chain.go
+++ http/roundtrip/chain/chain.go
@@ -1,7 +1,6 @@
 package chain
 
 import (
-	"log"
 	"net/http"
 	"sync"
 )
@@ -27,7 +26,7 @@ func (t *FirstTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	t.f.mu.Lock()
 	t.f.f = false
 	t.f.mu.Unlock()
-	log.Printf("#FirstTransport.flag: %v\n", t.f.f)
+	// log.Printf("#FirstTransport.flag: %v\n", t.f.f)
 	return ClassifyTransport(t.Transport).RoundTrip(req)
 }
 
@@ -40,7 +39,7 @@ func (t *SecondTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	t.f.mu.Lock()
 	t.f.f = true
 	defer t.f.mu.Unlock()
-	log.Printf("#SecondTransport.flag: %v\n", t.f.f)
+	// log.Printf("#SecondTransport.flag: %v\n", t.f.f)
 	return ClassifyTransport(t.Transport).RoundTrip(req)
 }
 
@@ -52,4 +51,39 @@ func NewChainedTransports(f *flag) *http.Client {
 			f:         f,
 		},
 	}
+}
+
+type hkey string
+type hval string
+
+func (k hkey) String() string { return string(k) }
+func (k hval) String() string { return string(k) }
+
+type headTransport struct {
+	Transport http.RoundTripper
+	h         map[hkey]hval
+}
+
+func (t *headTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	for k, v := range t.h {
+		req.Header.Add(k.String(), v.String())
+	}
+	// log.Printf("#headTransport.header: %v\n", req.Header)
+	return ClassifyTransport(t.Transport).RoundTrip(req)
+}
+
+type headTransportWithF struct {
+	Transport http.RoundTripper
+	h         map[hkey]hval
+	f         *flag
+}
+
+func (t *headTransportWithF) RoundTrip(req *http.Request) (*http.Response, error) {
+	for k, v := range t.h {
+		req.Header.Add(k.String(), v.String())
+	}
+	t.f.mu.Lock()
+	t.f.f = false
+	t.f.mu.Unlock()
+	return ClassifyTransport(t.Transport).RoundTrip(req)
 }
--- http/roundtrip/chain/chain_test.go
+++ http/roundtrip/chain/chain_test.go
@@ -34,3 +34,67 @@ func Test_ChainRoundtrip(t *testing.T) {
 		t.Fatalf("expected %q, got %q", "success!", v)
 	}
 }
+
+/*
+	$ go test -count 2 -benchmem -bench .
+	goos: darwin
+	goarch: amd64
+	pkg: github.com/nemotoy/golang-playground/http/roundtrip/chain
+	Benchmark_headTransport-4                   2740            511275 ns/op           26907 B/op        132 allocs/op
+	Benchmark_headTransport-4                   1137           1241009 ns/op           25685 B/op        130 allocs/op
+	Benchmark_headTransportNonChain-4            592           1774655 ns/op           25013 B/op        130 allocs/op
+	Benchmark_headTransportNonChain-4            549           1826221 ns/op           25245 B/op        131 allocs/op
+	PASS
+	ok      github.com/nemotoy/golang-playground/http/roundtrip/chain       5.704s
+*/
+func Benchmark_headTransport(b *testing.B) {
+
+	f := &flag{mu: &sync.Mutex{}}
+	hm := map[hkey]hval{
+		"keyA": "valA",
+	}
+	c := &http.Client{
+		Transport: &headTransport{
+			Transport: &FirstTransport{f: f},
+			h:         hm,
+		},
+	}
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte("success!"))
+	}))
+	defer ts.Close()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := c.Get(ts.URL)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func Benchmark_headTransportNonChain(b *testing.B) {
+
+	f := &flag{mu: &sync.Mutex{}}
+	hm := map[hkey]hval{
+		"keyA": "valA",
+	}
+	c := &http.Client{
+		Transport: &headTransportWithF{
+			h: hm,
+			f: f,
+		},
+	}
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte("success!"))
+	}))
+	defer ts.Close()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := c.Get(ts.URL)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
