3bb9872e474690d9a3ab6ea4a53bb009e354994f
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-06-22 21:57:38 +0900

    fix: prd code

--- di/main.go
+++ di/main.go
@@ -1,11 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"time"
+)
 
 type Message string
 
 type Greeter struct {
 	Message Message
+	Grumpy  bool
 }
 
 type Event struct {
@@ -17,15 +23,25 @@ func NewMessage() Message {
 }
 
 func NewGreeter(m Message) Greeter {
-	return Greeter{Message: m}
+	var grumpy bool
+	if time.Now().Unix()%2 == 0 {
+		grumpy = true
+	}
+	return Greeter{Message: m, Grumpy: grumpy}
 }
 
 func (g Greeter) Greet() Message {
+	if g.Grumpy {
+		return Message("Go away!")
+	}
 	return g.Message
 }
 
-func NewEvent(g Greeter) Event {
-	return Event{Greeter: g}
+func NewEvent(g Greeter) (Event, error) {
+	if g.Grumpy {
+		return Event{}, errors.New("could not create event")
+	}
+	return Event{Greeter: g}, nil
 }
 
 func (e Event) Start() {
@@ -34,7 +50,10 @@ func (e Event) Start() {
 }
 
 func main() {
-	e := InitializeEvent()
-
+	e, err := InitializeEvent()
+	if err != nil {
+		fmt.Printf("failed to create event: %s\n", err)
+		os.Exit(2)
+	}
 	e.Start()
 }
