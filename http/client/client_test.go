5e930506c1815382de4c787a5a0d3989f3d68960
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-06-25 22:45:07 +0900

    chore: add comment about closing request body

--- http/client/client_test.go
+++ http/client/client_test.go
@@ -14,6 +14,20 @@ type ss struct {
 	S string
 }
 
+/*
+	## When is the request body closed?
+	* request URL is nil
+	https://github.com/golang/go/blob/master/src/net/http/client.go#L596
+	* call the uerr and a reqBodyClosed is false. (ex, calls c.send() previously)
+	https://github.com/golang/go/blob/master/src/net/http/client.go#L617
+	* c.send() may be close req.Body
+	https://github.com/golang/go/blob/master/src/net/http/client.go#L719
+		* send() close req.Body when parameters are nil
+		https://github.com/golang/go/blob/334752dc8207d6d19d9fb1a99d2e97f7d326c82a/src/net/http/client.go#L204
+	* finish client.do()
+	https://github.com/golang/go/blob/master/src/net/http/client.go#L737
+
+*/
 func Test_Roundtrip(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		time.Sleep(100 * time.Millisecond)
@@ -34,7 +48,7 @@ func Test_Roundtrip(t *testing.T) {
 	c := &http.Client{Timeout: 1 * time.Millisecond}
 	resp, err := c.Do(req)
 	if err != nil {
-		// t.Fatalf("#Do: %#v", err)
+		// t.Fatalf("#Do; req: %v. error: %#v", req, err)
 		return
 	}
 	defer resp.Body.Close()
