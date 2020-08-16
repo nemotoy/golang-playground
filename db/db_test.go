925f8c18e7393ffcb93fe39eb4f98547a7662d4e
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-08-16 09:27:27 +0900

    chore: add test to learn re-use a db connection

---
+++ db/db_test.go
@@ -0,0 +1,49 @@
+package db
+
+import (
+	"database/sql"
+	"errors"
+	"reflect"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jinzhu/gorm"
+)
+
+// go1.14/src/database/sql/sql.go
+var errDBClosed = errors.New("sql: database is closed")
+
+// re-use a db connection.
+func Test_DB(t *testing.T) {
+	db, _, err := sqlmock.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	gDB, err := gorm.Open("mysql", db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var sqlDB *sql.DB = gDB.DB()
+
+	if !reflect.DeepEqual(db, sqlDB) {
+		t.Errorf("parse() got = %+v, but want = %+v", db, sqlDB)
+	}
+
+	db.Close()
+
+	if err := db.Ping(); err.Error() != errDBClosed.Error() {
+		t.Errorf("Ping() error: %+v", err)
+	}
+
+	if err := gDB.DB().Ping(); err.Error() != errDBClosed.Error() {
+		t.Errorf("Ping() error: %+v", err)
+	}
+
+	if err := sqlDB.Ping(); err.Error() != errDBClosed.Error() {
+		t.Errorf("Ping() error: %+v", err)
+	}
+
+	if !reflect.DeepEqual(db, sqlDB) {
+		t.Errorf("parse() got = %+v, but want = %+v", db, sqlDB)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module github.com/nemotoy/golang-playground
 go 1.14
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.3.3
 	github.com/davecgh/go-spew v1.1.1
 	github.com/google/go-cmp v0.4.1
 	github.com/google/go-github/v28 v28.1.1
 	github.com/google/wire v0.4.0
 	github.com/gorilla/mux v1.7.4
 	github.com/graphql-go/graphql v0.7.9
+	github.com/jinzhu/gorm v1.9.16
 	github.com/kr/pretty v0.2.0 // indirect
 	github.com/mitchellh/hashstructure v1.0.0
 	github.com/olekukonko/tablewriter v0.0.4
@@ -16,8 +18,7 @@ require (
 	github.com/stretchr/testify v1.5.1
 	go.uber.org/goleak v1.0.0
 	go.uber.org/multierr v1.5.0
-	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
-	golang.org/x/net v0.0.0-20200202094626-16171245cfb2
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e
 	golang.org/x/time v0.0.0-20170424234030-8be79e1e0910
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
