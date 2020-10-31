96ce7b3a0121d7cf616d84c256171fc1c308b2c6
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-10-31 23:05:16 +0900

    context: deadline

--- context/context_test.go
+++ context/context_test.go
@@ -3,6 +3,7 @@ package ctx
 import (
 	"context"
 	"testing"
+	"time"
 )
 
 type forCtxKey struct{}
@@ -14,6 +15,9 @@ type val struct {
 	i int
 }
 
+// go1.15/src/context/context.go:532
+// WithValue()のレシーバは、 valueCtxであり Context インターフェースが埋め込まれているため、Contextメソッドも利用できる実装である。
+// Contextインターフェースへの操作が実施されても、key/valueへの副作用は発生しない。(valueCtx, timerCtx, cancelCtx)
 func TestCtx(t *testing.T) {
 	ctx := context.Background()
 	in := &val{s: "sss", i: 100}
@@ -30,6 +34,20 @@ func TestCtx(t *testing.T) {
 
 func TestCtx_Cancel(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
+	in := &val{s: "sss", i: 100}
+	ctx = context.WithValue(ctx, ctxKey, in)
+	cancel()
+	out, ok := ctx.Value(ctxKey).(*val)
+	if !ok {
+		t.Fail()
+	}
+	if in != out {
+		t.Fail()
+	}
+}
+
+func TestCtx_DeadLine(t *testing.T) {
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(1*time.Second))
 	in := &val{s: "sss", i: 100}
 	ctx = context.WithValue(ctx, ctxKey, in)
 	cancel()
