497d41c6c9a0c016e8229e9e336a40bfde5e97d8
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-12-08 23:45:09 +0900

    http/test: add user api

--- http/test/a_test.go
+++ http/test/a_test.go
@@ -1,6 +1,7 @@
 package examples
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -14,25 +15,50 @@ func ping(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("pong\n"))
 }
 
-func PingHandler() http.Handler {
+type userImpl struct {
+}
+
+type User struct {
+	Name string `json:"name"`
+}
+
+func (u *userImpl) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	res := User{Name: "hoge"}
+	b, _ := json.Marshal(res)
+	_, _ = w.Write(b)
+}
+
+func initHandler() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		ping(w, r)
 	})
+	mux.Handle("/user", new(userImpl))
 	return mux
 }
 
 func TestPing(t *testing.T) {
-	handler := PingHandler()
+	handler := initHandler()
 
 	server := httptest.NewServer(handler)
 	defer server.Close()
 
 	e := httpexpect.New(t, server.URL)
 
-	e.GET("/ping").
-		Expect().
-		Status(http.StatusOK).ContentType("text/plain").Text().Equal("pong\n")
+	{
+		e.GET("/ping").
+			Expect().
+			Status(http.StatusOK).ContentType("text/plain").Text().Equal("pong\n")
+	}
+	{
+		raw := e.GET("/user").
+			Expect().
+			Status(http.StatusOK).ContentType("application/json").JSON().Object()
+		raw.ContainsMap(map[string]interface{}{
+			"name": "hoge",
+		})
+	}
 }
 
 func TestMain(m *testing.M) {
