8c64378ffe45db059a98f5a3fd79653225b3ec23
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-02-28 15:10:19 +0900

    clis/tui: starred_at

--- clis/tui/github/client.go
+++ clis/tui/github/client.go
@@ -13,8 +13,14 @@ import (
 type user struct {
 	Name                githubv4.String
 	StarredRepositories struct {
+		Edges []starredRepositoritoryEdge
 		Nodes []repository
 	} `graphql:"starredRepositories(last: $starredRepositoriesLast)"`
+}
+
+// represents StarredRepositoryEdge object.
+type starredRepositoritoryEdge struct {
+	StarredAt githubv4.DateTime
 }
 
 // represents Repository object.
@@ -49,7 +55,7 @@ func main() {
 		os.Exit(1)
 	}
 	for _, v := range query.Viewer.Following.Nodes {
-		fmt.Printf("name: %s, edges:\n", v.Name, v.StarredRepositories.Edges)
+		fmt.Printf("name: %s, edges:%+v\n", v.Name, v.StarredRepositories.Edges)
 		for _, st := range v.StarredRepositories.Nodes {
 			fmt.Printf("URL: %s\n", st.URL)
 		}
