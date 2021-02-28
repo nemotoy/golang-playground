0ff3afcdb653f3923151ccf37698c4889aec3564
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-02-28 12:05:14 +0900

    clis/tui: query that find following commits

--- clis/tui/github/client.go
+++ clis/tui/github/client.go
@@ -16,11 +16,32 @@ func main() {
 	httpClient := oauth2.NewClient(context.Background(), src)
 
 	client := githubv4.NewClient(httpClient)
-	var query struct{}
+
+	var query struct {
+		Viewer struct {
+			Name      githubv4.String
+			Following struct {
+				Nodes []struct {
+					Name           githubv4.String
+					CommitComments struct {
+						Nodes []struct {
+							URL         githubv4.URI
+							PublishedAt githubv4.DateTime
+							CreatedAt   githubv4.DateTime
+							UpdatedAt   githubv4.DateTime
+						}
+					} `graphql:"commitComments(last: 5)"`
+				}
+			} `graphql:"following(last: 5)"`
+		}
+	}
+
 	err := client.Query(context.Background(), &query, nil)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Println("Data: ", query)
+	for _, v := range query.Viewer.Following.Nodes {
+		fmt.Printf("Name: %s, Commits: %+v\n", v.Name, v.CommitComments)
+	}
 }
