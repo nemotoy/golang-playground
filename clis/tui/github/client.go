4f5c39deb89c9681a391d962d067ae46265b9321
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-03-01 22:07:10 +0900

    clis/tui: language

--- clis/tui/github/client.go
+++ clis/tui/github/client.go
@@ -21,9 +21,10 @@ type user struct {
 		Nodes []repository
 	} `graphql:"topRepositories(last: $topRepositoriesLast, orderBy: {field: CREATED_AT, direction: ASC})"`
 	ContributionsCollection struct {
-		TotalCommitContributions      githubv4.Int
-		TotalIssueContributions       githubv4.Int
-		TotalPullRequestContributions githubv4.Int
+		CommitContributionsByRepository []commitContributionsByRepository
+		TotalCommitContributions        githubv4.Int
+		TotalIssueContributions         githubv4.Int
+		TotalPullRequestContributions   githubv4.Int
 	} `graphql:"contributionsCollection(from: $contributionsCollectionFrom, to: $contributionsCollectionTo)"`
 }
 
@@ -35,6 +36,31 @@ type starredRepositoritoryEdge struct {
 // represents Repository object.
 type repository struct {
 	URL githubv4.URI
+}
+
+type commitContributionsByRepository struct {
+	Contributions struct {
+		Nodes      []createdCommitContribution
+		TotalCount githubv4.Int
+	} `graphql:"contributions(last:10)"`
+}
+
+type createdCommitContribution struct {
+	CommitCount githubv4.Int
+	OccurredAt  githubv4.DateTime
+	URL         githubv4.URI
+	Repository  repositoryPerCommit
+}
+
+type repositoryPerCommit struct {
+	Languages struct {
+		Nodes []language
+	} `graphql:"languages(last:10)"`
+}
+
+type language struct {
+	Color githubv4.String
+	Name  githubv4.String
 }
 
 func main() {
@@ -72,6 +98,7 @@ func main() {
 		os.Exit(1)
 	}
 	for _, v := range query.Viewer.Following.Nodes {
+		fmt.Println("+++++++++++++++++++++++++++++++++++++++++++++++++++++")
 		fmt.Printf("name: %s, edges:%+v\n", v.Name, v.StarredRepositories.Edges)
 		for _, st := range v.StarredRepositories.Nodes {
 			fmt.Printf("URL: %s\n", st.URL)
@@ -80,5 +107,7 @@ func main() {
 			fmt.Printf("URL: %+v\n", tr)
 		}
 		fmt.Printf("Contribution count: %+v\n", v.ContributionsCollection)
+		fmt.Printf("Commit contribution: %+v\n", v.ContributionsCollection.CommitContributionsByRepository)
+		fmt.Println("+++++++++++++++++++++++++++++++++++++++++++++++++++++")
 	}
 }
