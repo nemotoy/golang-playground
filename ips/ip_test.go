197aaaaf1fe592b5b8c5ab5d0a2be3a1e8448508
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-04-27 12:31:54 +0900

    ip: parsing ip

---
+++ ips/ip_test.go
@@ -0,0 +1,26 @@
+package ips
+
+import (
+	"net"
+	"testing"
+)
+
+func TestParseIp(t *testing.T) {
+	tests := []struct {
+		ip   string
+		want bool
+	}{
+		{"test", false},
+		{"192.168.5.1", true},
+		{"127.0.0.1/32", true},
+		{"0000:0000:0000:0000:0000:ffff:7f00:0001", true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.ip, func(t *testing.T) {
+			ip := net.ParseIP(tt.ip)
+			if ip == nil && tt.want {
+				t.Error("unexpeccted")
+			}
+		})
+	}
+}
