9afa337e0c9a6c8a94dfcd4f3f900548dfce53fb
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-09-05 08:13:55 +0900

    construct user

--- ddd/server/domain/model/user.go
+++ ddd/server/domain/model/user.go
@@ -1,5 +1,9 @@
 package model
 
+import (
+	"fmt"
+)
+
 type User struct {
 	LastName  LastName
 	FirstName FirstName
@@ -7,4 +11,27 @@ type User struct {
 
 type LastName string
 
+func (ln LastName) satisfiedLength() bool {
+	l := len(ln)
+	return 0 < l && l <= 128
+}
+
 type FirstName string
+
+func (ln FirstName) satisfiedLength() bool {
+	l := len(ln)
+	return 0 < l && l <= 128
+}
+
+func NewUser(lastName LastName, firstName FirstName) (*User, error) {
+	if !lastName.satisfiedLength() {
+		return nil, fmt.Errorf("")
+	}
+	if !firstName.satisfiedLength() {
+		return nil, fmt.Errorf("")
+	}
+	return &User{
+		LastName:  lastName,
+		FirstName: firstName,
+	}, nil
+}
