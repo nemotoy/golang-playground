e1f7bebf694c2501c3fab03be6cf555b0a4e8d0c
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-09-04 09:57:57 +0900

    tmp

---
+++ ddd/server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", ping)
+	srv := &http.Server{
+		Addr:    ":8085",
+		Handler: mux,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			log.Println("failed to serve: ", err)
+		}
+	}()
+
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, syscall.SIGTERM, os.Interrupt)
+	<-s
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Println("failed to shutdown gracefully: ", err)
+	}
+	log.Println("Server shutdown")
+}
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Received Request: %v\n", r)
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("pong\n"))
+}
